200af7bc51e67f5fae2eea58ba77b9340ec1fd66
umer <syedumerahmedcode@gmail.com>
2026-01-30 21:43:04 +0100

    Feature: Implemented the solution and tested it against a valid test case scenario.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,30 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        /**
+         * Given n nodes labeled from 0 to n-1 and a list of undirected edges
+         * (each edge is a pair of nodes), write a function to check whether
+         * these edges make up a valid tree.
+        
+         *          *Example 1:
+        
+         *          *Input: n = 5, and edges = [[0,1], [0,2], [0,3], [1,4]] Output: true
+         * Example 2:
+        
+         *          *Input: n = 5, and edges = [[0,1], [1,2], [2,3], [1,3], [1,4]] Output:
+         * false Note: you can assume that no duplicate edges will appear in
+         * edges. Since all edges are undirected, [0,1] is the same as [1,0] and
+         * thus will not appear together in edges.
+         */
+        int n = 5; // Number of nodes
+        int[][] edges = {
+            {0, 1},
+            {0, 2},
+            {0, 3},
+            {1, 4}
+        }; // 4 edges for 5 nodes
+        Solution solution = new Solution();
+        boolean result = solution.validTree(n, edges);
+        System.out.println("Is the graph constrcuted above a valid tree: " +result);
     }
 }
--- src/Solution.java
+++ src/Solution.java
@@ -1,4 +1,69 @@
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Stack;
+
+
 public class Solution {
+
+    public boolean validTree(int n, int[][] edges) {
+        /**
+         * In order to check if the given graph is a valid tree, we have to check two things:
+         * 1) There is no cycle among the nodes/vertices
+         * 2) All verzices/nodes are connected
+         */
+        /**
+         * If the difference between nodes and edges is not 1[nodes  have a 1 more value then edges],
+                 * then this is not a valid tree and we return false immediately
+         */
+        if (edges.length != n - 1) {
+            return false;
+        }
+        /**
+         * We create an adjacency List to check through
+         */
+        List<List<Integer>> adjacencyList = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            // here n is treated as vertices
+            adjacencyList.add(new ArrayList<>());
+        }
+        for (int[] edge : edges) {
+            /**
+             * We have to add two entries to create an undirected graph, as in, 
+             * if(0--1),
+                     * then (0--->1) and (1--->0)
+             */
+            adjacencyList.get(edge[0]).add(edge[1]);
+            adjacencyList.get(edge[1]).add(edge[0]);
+        }
+
+        /**
+         * Doing iterative dfs
+         */
+        Stack<Integer> stack = new Stack<>();
+        HashSet<Integer> visited = new HashSet<>();
+
+        stack.push(0);
+        visited.add(0);
+
+        while (!stack.isEmpty()) {
+            int node = stack.pop();
+            for (int neighbour : adjacencyList.get(node)) {
+                if (visited.contains(neighbour)) {
+                    // do nothing
+                    continue;
+                }
+                visited.add(neighbour);
+                stack.push(neighbour);
+            }
+        }
+        if (visited.size() == n) {
+            return true;
+        }
+        return false;
+
+        
+    }
 
 }
